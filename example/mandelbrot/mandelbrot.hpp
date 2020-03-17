754be0d45a2382f66a663830f4e3f7c4fec78031
Christopher Kormanyos <ckormanyos@yahoo.com>
2020-03-17 09:48:53 +0100

    Namespaces to improve program grouping

--- example/mandelbrot/mandelbrot.hpp
+++ example/mandelbrot/mandelbrot.hpp
@@ -46,71 +46,76 @@
 #include <boost/gil/typedefs.hpp>
 #include <boost/lexical_cast.hpp>
 
-namespace my_concurrency
+namespace boost { namespace multiprecision { namespace mandelbrot {
+
+namespace detail {
+
+namespace my_concurrency {
+template<typename index_type,
+          typename callable_function_type>
+void parallel_for(index_type             start,
+                  index_type             end,
+                  callable_function_type parallel_function)
 {
-  template<typename index_type,
-            typename callable_function_type>
-  void parallel_for(index_type             start,
-                    index_type             end,
-                    callable_function_type parallel_function)
-  {
-    // Estimate the number of threads available.
-    static const unsigned int number_of_threads_hint =
-      std::thread::hardware_concurrency();
+  // Estimate the number of threads available.
+  static const unsigned int number_of_threads_hint =
+    std::thread::hardware_concurrency();
 
-    static const unsigned int number_of_threads =
-      ((number_of_threads_hint == 0U) ? 4U : number_of_threads_hint);
+  static const unsigned int number_of_threads =
+    ((number_of_threads_hint == 0U) ? 4U : number_of_threads_hint);
 
-    // Set the size of a slice for the range functions.
-    index_type n = index_type(end - start) + index_type(1);
+  // Set the size of a slice for the range functions.
+  index_type n = index_type(end - start) + index_type(1);
 
-    index_type slice =
-      static_cast<index_type>(std::round(n / static_cast<double> (number_of_threads)));
+  index_type slice =
+    static_cast<index_type>(std::round(n / static_cast<double> (number_of_threads)));
 
-    slice = (std::max)(slice, index_type(1));
+  slice = (std::max)(slice, index_type(1));
 
-    // Inner loop.
-    auto launch_range =
-      [&parallel_function](index_type index_lo, index_type index_hi)
+  // Inner loop.
+  auto launch_range =
+    [&parallel_function](index_type index_lo, index_type index_hi)
+    {
+      for(index_type i = index_lo; i < index_hi; ++i)
       {
-        for(index_type i = index_lo; i < index_hi; ++i)
-        {
-          parallel_function(i);
-        }
-      };
-
-    // Create the thread pool and launch the jobs.
-    std::vector<std::thread> pool;
-
-    pool.reserve(number_of_threads);
-
-    index_type i1 = start;
-    index_type i2 = (std::min)(index_type(start + slice), end);
-
-    for(index_type i = 0U; ((index_type(i + index_type(1U)) < number_of_threads) && (i1 < end)); ++i)
-    {
-      pool.emplace_back(launch_range, i1, i2);
-
-      i1 = i2;
-
-      i2 = (std::min)(index_type(i2 + slice), end);
-    }
-
-    if(i1 < end)
-    {
-      pool.emplace_back(launch_range, i1, end);
-    }
-
-    // Wait for the jobs to finish.
-    for(std::thread& thread_in_pool : pool)
-    {
-      if(thread_in_pool.joinable())
-      {
-        thread_in_pool.join();
+        parallel_function(i);
       }
+    };
+
+  // Create the thread pool and launch the jobs.
+  std::vector<std::thread> pool;
+
+  pool.reserve(number_of_threads);
+
+  index_type i1 = start;
+  index_type i2 = (std::min)(index_type(start + slice), end);
+
+  for(index_type i = 0U; ((index_type(i + index_type(1U)) < number_of_threads) && (i1 < end)); ++i)
+  {
+    pool.emplace_back(launch_range, i1, i2);
+
+    i1 = i2;
+
+    i2 = (std::min)(index_type(i2 + slice), end);
+  }
+
+  if(i1 < end)
+  {
+    pool.emplace_back(launch_range, i1, end);
+  }
+
+  // Wait for the jobs to finish.
+  for(std::thread& thread_in_pool : pool)
+  {
+    if(thread_in_pool.joinable())
+    {
+      thread_in_pool.join();
     }
   }
+}
 } // namespace my_concurrency
+
+} // namespace detail
 
 // Declare a base class for the Mandelbrot configuration.
 template<typename NumericType,
@@ -296,7 +301,7 @@ public:
 
     static const NumericType four(4U);
 
-    my_concurrency::parallel_for
+    detail::my_concurrency::parallel_for
     (
       std::size_t(0U),
       y_values.size(),
@@ -456,5 +461,7 @@ private:
   std::vector<std::vector<std::uint_fast32_t>> mandelbrot_iteration_matrix;
   std::vector<std::uint_fast32_t>              mandelbrot_color_histogram;
 };
+
+} } } // namespace boost::multiprecision::mandelbrot
 
 #endif // BOOST_MULTIPRECISION_MANDELBROT_2020_03_12_HPP_
